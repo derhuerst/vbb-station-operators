b1112e068120c110a470d7e449e59753bd60f88d
Jannis R <mail@jannisr.de>
2017-06-21 16:26:17 +0200

    implementation :clap:, tests :white_check_mark:

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict'
 
-// todo
+const {operators, stations} = require('./data.json')
+
+const operatedBy = {}
+
+for (let id in stations) {
+	operatedBy[id] = operators[stations[id]]
+}
+
+module.exports = operatedBy
--- test.js
+++ test.js
@@ -4,6 +4,18 @@ const test = require('tape')
 
 const operatedBy = require('.')
 
-test('operatedBy', (t) => {
-	// todo
+test('each operator should have an id and a name', (t) => {
+	for (let station in operatedBy) {
+		// todo: validate station ID
+
+		const o = operatedBy[station]
+
+		t.equal(typeof o.id, 'string')
+		t.ok(o.id)
+
+		t.equal(typeof o.name, 'string')
+		t.ok(o.name)
+	}
+
+	t.end()
 })
